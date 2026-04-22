943fead4d3dc4870212fa48fbe09fa4d350fd995
miklovis <39951875+miklovis@users.noreply.github.com>
2026-04-22 22:07:22 +0100

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result = 0;
+        foreach (int num in nums) {
+            result = num ^ result;
+        }
+
+        return result;
+    }
+}
