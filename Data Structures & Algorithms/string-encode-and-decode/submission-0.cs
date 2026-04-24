d77f1f0c7169b7874c8406f1b8ad091bb0325908
miklovis <39951875+miklovis@users.noreply.github.com>
2026-04-24 20:09:47 +0100

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cs
@@ -0,0 +1,28 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        string encoded = "";
+        
+        foreach (string str in strs) {
+            encoded += str;
+            encoded += ';';
+        }
+
+        Console.WriteLine(encoded);
+        return encoded;
+    }
+
+    public List<string> Decode(string s) {
+        var decoded = new List<string>();
+
+        while(s.IndexOf(';') != -1) {
+            int delimiterIndex = s.IndexOf(';');
+
+            decoded.Add(s.Substring(0, delimiterIndex));
+
+            s = s.Substring(delimiterIndex + 1);
+        }
+
+        return decoded;
+   }
+}
